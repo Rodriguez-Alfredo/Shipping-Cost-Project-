ab735236cfa8be50f74b794c51a44f50104ef179
AlfredoRodriguez <126662592+Rodriguez-Alfredo@users.noreply.github.com>
2023-03-17 21:36:21 +0000

    Made the cost per weight a variable. Also added a string to the total price in line 18.

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 #!/bin/python3
 
+#Price per weight
+shipping_cost = 1.20
+
 #User enter the cost of the item
 customer_basket_cost = float(input('Please enter cost: '))
 
@@ -11,7 +14,7 @@ if customer_basket_cost > 100:
   print('Free Shippment!')
   
 else:
-  customer_basket_cost = customer_basket_cost + (customer_basket_weight * 1.20)
-  print(customer_basket_cost)
+  customer_basket_cost = customer_basket_cost + (customer_basket_weight * shipping_cost)
+  print('Your total cost is: $' + str(customer_basket_cost))
 
 #Source: TryHackMe.com
